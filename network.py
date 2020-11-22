dda45657982e7c13df27833da4153661582e528c
James Barlow <jw.barlow@outlook.com>
2020-11-21 23:14:29 -0500

    Some network changes.

--- network.py
+++ network.py
@@ -1,46 +1,59 @@
 import socket
 import pickle
 import main
-
+import threading
+import queue
 
 class Network:
-    """Contains functions to connect and send data"""
+	"""Contains functions to connect and send data"""
 
-    def __init__(self, server_ip: str, port: int):
-        """
-        Initializes an object for a connection to a computer
+	def __init__(self, server_ip: str, port: int):
+		"""
+		Initializes an object for a connection to a computer
 
 
-        client -- The client socket file descriptor
-        server -- The server internet protocol address
-        port -- The port we are connecting too
-        addr -- Comprised of server and port
-        id -- The client id
-        """
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = server_ip
-        self.port = port
-        self.addr = (self.server, self.port)
-        #self.id = self.connect()
-        #print(self.id)
+		client -- The client socket file descriptor
+		server -- The server internet protocol address
+		port -- The port we are connecting too
+		addr -- Comprised of server and port
+		id -- The client id
+		"""
+		self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.server = server_ip
+		self.port = port
+		self.addr = (self.server, self.port)
+		self.xqu = queue.Queue()
 
-    def connect(self):
-        """
-        Connects to the game server        
-        """
-        try:
-            self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
-        except:
-            pass
+	def listener(self):
+		while True:
+			rec = pickle.loads(self.client.recv(2048))
+			self.xqu.put(rec)
 
-    def send(self, data):
-        """
-        Sends data to the game server
-        """
-        try:
-            data_stream = pickle.dumps(data)
-            self.client.send(data_stream)
-            return self.client.recv(2048)
-        except socket.error as e:
-            print(e)
+	def connect(self):
+		"""
+		Connects to the game server        
+		"""
+		try:
+			self.client.connect(self.addr)
+			if self.client.recv(2048).decode() == "Connected":
+				threading.Thread(target=Network.listener,args=(self)).start()
+			else:
+				self.client.shutdown()
+		except:
+			pass
+
+	def receive(self):
+		if self.xqu.empty:
+			return None
+		return self.xqu.get_nowait()
+
+	def send(self, data):
+		"""
+		Sends data to the game server
+		"""
+		try:
+			data_stream = pickle.dumps(data)
+			self.client.send(data_stream)
+			return self.client.recv(2048)
+		except socket.error as e:
+			print(e)
--- server.py
+++ server.py
@@ -5,49 +5,54 @@ import pickle
 from _thread import *
 import sys
 import os
-
+import network
 
 # hostname = socket.gethostname()  
 # server = socket.gethostbyname(hostname) 
-server = "127.0.0.1"
+server = "0.0.0.0"
 port = 5969
 
+def start_server_thread():
+	start_new_thread(start_server)
+	ret = network.Network("127.0.0.1",port)
+	return ret
+
 def start_server():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    try:
-        s.bind((server, port))
-    except socket.error as e:
-        str(e)
+	try:
+		s.bind((server, port))
+	except socket.error as e:
+		str(e)
 
-    s.listen(3) # Limited to 3 connections
-    print("Waiting for a connection, Server Started")
-    while True:
-        conn, addr = s.accept()
-        #print("Connected to:", addr)
-        start_new_thread(threaded_client, (conn,))
+	s.listen(3) # Limited to 3 connections
+	print("Waiting for a connection, Server Started")
+	while True:
+		conn, addr = s.accept()
+		#print("Connected to:", addr)
+		start_new_thread(threaded_client, (conn,))
 
 def threaded_client(conn):
-    conn.send(str.encode("Connected"))
-    # os.system("cls" if os.name == "nt" else "clear")
-    # main.board.print_board()
-    while True:
-        try:
-            data = conn.recv(2048)
-            data_variable = pickle.loads(data)
+	conn.send(str.encode("Connected"))
+	# os.system("cls" if os.name == "nt" else "clear")
+	# main.board.print_board()
+	while True:
+		try:
+			data = conn.recv(2048)
+			data_variable = pickle.loads(data)
 
-            if not data:
-                print("Disconnected")
-                break
-            else:
-                print("Recieved Something")
-                print(data_variable)
-            
-            conn.sendall(data_variable)
-        except:
-            break
+			if not data:
+				print("Disconnected")
+				break
+			else:
+				print("Recieved Something")
+				print(data_variable)
+			
+			conn.sendall(data_variable)
+		except:
+			break
 
-    print("Lost connection")
-    conn.close()
+	print("Lost connection")
+	conn.close()
 
 
